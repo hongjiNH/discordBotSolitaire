5476ec751634096f2debc41e9286e833f9f6ac5e
hongjiNH <hongjichai@gmail.com>
2023-09-10 13:13:54 +0800

    create a dev js

---
+++ indexDev.js
@@ -0,0 +1,136 @@
+require('dotenv').config({ path: ".env.dev" });
+const fs = require('node:fs');
+const path = require('node:path');
+const changeStatus = require('./botActivity/botActivity')
+const commonVariable = require('./share/index');
+const autoUpdate = require('./deploy-commands')
+
+const { Client, Collection, Events, GatewayIntentBits,EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+
+const token = process.env.token;
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+// Grab all the command folders from the commands directory you created earlier
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+// Grab all the command/clash of clan folders from the commands directory you created earlier
+const clashOfClanPath = path.join(__dirname, 'commands/clashOfClan');
+const clashOfClanCommandFolders = fs.readdirSync(clashOfClanPath);
+
+// // Grab all the command/valorent folders from the commands directory you created earlier
+// const valorentPath = path.join(__dirname, 'commands/valorent');
+// const valorentCommandFolders = fs.readdirSync(valorentPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+
+}
+
+// for coc command
+for (const folder of clashOfClanCommandFolders) {
+	const commandsPath = path.join(clashOfClanPath, folder);
+
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+
+}
+
+// // for valorent command
+// for (const folder of valorentCommandFolders) {
+
+// 	const commandsPath = path.join(valorentPath, folder);
+	
+// 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+// 	for (const file of commandFiles) {
+// 		const filePath = path.join(commandsPath, file);
+// 		const command = require(filePath);
+// 		if ('data' in command && 'execute' in command) {
+// 			client.commands.set(command.data.name, command);
+
+// 		} else {
+// 			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+// 		}
+// 	}
+
+// }
+
+client.once(Events.ClientReady, c => {
+
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+
+	const Guilds = client.guilds.cache.map(guild => guild.id);
+
+	autoUpdate(Guilds,client);
+
+	changeStatus(client, Guilds);
+
+	setInterval(() => {
+		changeStatus(client, Guilds);
+	}, 2 * 60 * 60 * 1000);
+
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+	if (!command) return;
+
+	try {
+		await command.execute(interaction, client);
+	} catch (error) {
+
+		console.error(error);
+
+		const errorEmbed = new EmbedBuilder()
+			.setColor(commonVariable.errorEmbedColorCode)
+			.setTimestamp()
+			.setFooter(commonVariable.embedFooter)
+
+			.setTitle("Support server")
+			.setDescription("Contact the support by joining ")
+
+		const urlButton = new ButtonBuilder()
+			.setLabel('Join now')
+			.setURL(commonVariable.supportLink)
+			.setStyle(ButtonStyle.Link);
+
+		const row = new ActionRowBuilder()
+			.addComponents(urlButton);
+
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ embeds: [errorEmbed], files: [commonVariable.file], ephemeral: true, components: [row] });
+		} else {
+			await interaction.reply({ embeds: [errorEmbed], files: [commonVariable.file], ephemeral: true, components: [row] });
+		}
+	}
+});
+
+client.login(token);
